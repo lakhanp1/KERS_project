a9754348e50c5db82cf30c898de30bafdbd7e9e7
Lakhansing Pardeshi <lakhanp1@gmail.com>
2021-01-13 17:59:35 +0800

    updated renv setup

--- scripts/00_project_env.R
+++ scripts/00_project_env.R
@@ -1,3 +1,12 @@
+suppressPackageStartupMessages(library(devtools))
+suppressPackageStartupMessages(library(topGO))
+suppressPackageStartupMessages(library(scales))
+suppressPackageStartupMessages(library(KEGGprofile))
+suppressPackageStartupMessages(library(clusterProfiler))
+suppressPackageStartupMessages(library(tm))  # for text mining
+suppressPackageStartupMessages(library(SnowballC)) # for text stemming
+suppressPackageStartupMessages(library(wordcloud)) # word-cloud generator
+suppressPackageStartupMessages(library(configr)) # word-cloud generator
 suppressPackageStartupMessages(library(renv))
 
 # initialize a new project-local environment with a private R library
@@ -11,4 +20,7 @@ renv::history()
 
 # restore the state of your project from renv.lock
 renv::restore()
+
+
+renv::deactivate()
 
