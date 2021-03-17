d877d1c6446d3576a5b1afe65aa5d5b463a5e946
Lakhansing Pardeshi <lakhanp1@gmail.com>
2021-03-18 00:48:42 +0800

    major changes and bug fix
    bug: polII LFC was calculated based on ratio of already log2 values. Fixed it.
    using config for polII ratio
    replaced data.table::melt with dplyr::pivot_longer

--- scripts/15_KERS_SM_polII_FC.R
+++ scripts/15_KERS_SM_polII_FC.R
@@ -1,134 +1,189 @@
-library(chipmine)
-library(org.Anidulans.FGSCA4.eg.db)
-library(esquisse)
-library(summarytools)
-library(here)
-library(ggpubr)
+suppressPackageStartupMessages(library(chipmine))
+suppressPackageStartupMessages(library(org.Anidulans.FGSCA4.eg.db))
+suppressPackageStartupMessages(library(here))
+suppressPackageStartupMessages(library(ggpubr))
 
-
+## Tile plot showing KERS binding and log2(mutant/WT) for each of the SM clusters gene
 
 rm(list = ls())
 
 ##################################################################################
-analysisName <- "KERS_20h_polII_FC"
-outDir <- here::here("kdmB_analysis", "SM_analysis", "KERS_20h_SM")
+analysisName <- "binding_vs_del_polII_LFC_20h"
+outDir <- here::here("analysis", "07_SM_analysis", "KERS_20h_SM")
 
 outPrefix <- paste(outDir, "/", analysisName, sep = "")
 
 tfIds <- c("An_kdmB_20h_HA_1", "An_ecoA_20h_HA_1", "An_rpdA_20h_HA_1", "An_sntB_20h_HA_1")
-polII_ids <- c("An_untagged_20h_polII_1", "An_untagged_48h_polII_1",
-               "An_kdmB_del_20h_polII_1", "An_kdmB_del_48h_polII_1")
+polIIComparison <- "polII_20h.kdmB_del_vs_untagged"
 
-polIIPairId <- "p3"
 
-## polII signal fold change pairs
-polIIDiffPairs <- list(
-  # p1 = list(
-  #   name = "48h_vs_20h_untagged_polII",
-  #   title = "SM genes | KERS peaks | polII log2(untagged_48h \n vs untagged_20h)",
-  #   samples = c("An_untagged_20h_polII_1", "An_untagged_48h_polII_1")
-  # ),
-  # p2 = list(
-  #   name = "48h_vs_20h_kdmB_del_polII",
-  #   title = "SM genes | KERS peaks | polII log2(kdmB_del_48h \n vs kdmB_del_20h)",
-  #   samples = c("An_kdmB_del_20h_polII_1", "An_kdmB_del_48h_polII_1")
-  # ),
-  p3 = list(
-    name = "kdmB_del_vs_untagged_20h_polII",
-    title = "SM genes | KERS peaks | polII log2(kdmB_del \n vs kdmB_untagged 20h)",
-    samples = c("An_untagged_20h_polII_1", "An_kdmB_del_20h_polII_1")
-  # ),
-  # p4 = list(
-  #   name = "kdmB_del_vs_untagged_48h_polII",
-  #   title = "SM genes | KERS peaks | polII log2(kdmB_del \n vs kdmB_untagged 48h)",
-  #   samples = c("An_untagged_48h_polII_1", "An_kdmB_del_48h_polII_1")
-  )
-)
+file_polIIRatioConf <- here::here("data", "reference_data", "polII_ratio.config.tab")
 
-plotTitle <- paste("SM genes | KERS targets |", polIIDiffPairs[[polIIPairId]]$name)
+## genes to read
+file_exptInfo <- here::here("data", "reference_data", "sampleInfo.txt")
+file_genes <- here::here("data", "reference_data", "AN_genesForPolII.bed")
+file_topGoMap <- "E:/Chris_UM/Database/A_Nidulans/annotation_resources/geneid2go.ANidulans.topGO.map"
 
-file_exptInfo <- here::here("data", "referenceData/sampleInfo.txt")
-file_genes <- here::here("data", "referenceData/AN_genesForPolII.bed")
-file_topGoMap <- "E:/Chris_UM/Database/A_Nidulans/ANidulans_OrgDb/geneid2go.ANidulans.topGO.map"
-
-TF_dataPath <- here::here("data", "TF_data")
-polII_dataPath <- here::here("data", "polII_data")
-hist_dataPath <- here::here("data", "histone_data")
+TF_dataPath <- here::here("..", "data", "A_nidulans", "TF_data")
+polII_dataPath <- here::here("..", "data", "A_nidulans", "polII_data")
+hist_dataPath <- here::here("..", "data", "A_nidulans", "histone_data")
+other_dataPath <- here::here("..", "data", "A_nidulans", "other_data")
 
 orgDb <- org.Anidulans.FGSCA4.eg.db
 
-if(!dir.exists(outDir)){
-  dir.create(outDir)
-}
+# "deeptools", "miao", "normalizedmatrix", "normalizedmatrix_5kb"
+matrixType <- "normalizedmatrix_5kb"
+matrixDim = c(500, 200, 100, 10)
 
 ##################################################################################
+if(!dir.exists(outDir)) dir.create(path = outDir, recursive = TRUE)
+
 ## genes to read
-geneSet <- data.table::fread(file = file_genes, header = F,
-                             col.names = c("chr", "start", "end", "gene", "score", "strand")) %>% 
-  dplyr::mutate(length = end - start)
+geneSet <- suppressMessages(
+  readr::read_tsv(file = file_genes, col_names = c("chr", "start", "end", "geneId", "score", "strand"))
+) %>% 
+  dplyr::select(geneId)
 
-geneDesc <- AnnotationDbi::select(x = orgDb, keys = geneSet$gene, columns = c("SM_CLUSTER"), keytype = "GID")
-
-geneSet <- dplyr::left_join(x = geneSet, y = geneDesc, by = c("gene" = "GID"))
-
-##################################################################################
-
-tfInfo <- get_sample_information(exptInfoFile = file_exptInfo,
-                                 samples = tfIds,
-                                 dataPath = TF_dataPath,
-                                 matrixSource = "normalizedmatrix")
-
-polII_info <- get_sample_information(exptInfoFile = file_exptInfo,
-                                     samples = polII_ids,
-                                     dataPath = polII_dataPath,
-                                     matrixSource = "normalizedmatrix")
-
-exptData <- dplyr::bind_rows(tfInfo, polII_info)
-exptDataList <- purrr::transpose(exptData) %>% 
-  purrr::set_names(nm = purrr::map(., "sampleId"))
-
-polIICols <- list(
-  exp = structure(polII_info$sampleId, names = polII_info$sampleId),
-  is_expressed = structure(paste("is_expressed", ".", polII_info$sampleId, sep = ""), names = polII_info$sampleId)
+geneDesc <- suppressMessages(
+  AnnotationDbi::select(x = orgDb, keys = geneSet$geneId,
+                        columns = c("DESCRIPTION"), keytype = "GID")
 )
 
+smInfo <- suppressMessages(
+  AnnotationDbi::select(x = orgDb, keys = keys(orgDb, keytype = "SM_CLUSTER"),
+                        columns = c("GID", "SM_ID"), keytype = "SM_CLUSTER")) %>% 
+  dplyr::group_by(GID) %>% 
+  dplyr::mutate(SM_CLUSTER = paste(SM_CLUSTER, collapse = ";"),
+                SM_ID = paste(SM_ID, collapse = ";")) %>% 
+  dplyr::slice(1L) %>% 
+  dplyr::ungroup()
+
+geneInfo <- dplyr::left_join(x = geneSet, y = geneDesc, by = c("geneId" = "GID")) %>% 
+  dplyr::left_join(y = smInfo, by = c("geneId" = "GID"))
+
+glimpse(geneInfo)
+
+## polII ratio configuration
+polIIRatioConf <- suppressMessages(readr::read_tsv(file = file_polIIRatioConf)) %>% 
+  dplyr::filter(comparison %in% polIIComparison)
+
+polIIDiffPairs <- purrr::transpose(polIIRatioConf)  %>% 
+  purrr::set_names(nm = purrr::map(., "comparison"))
+
+
+##################################################################################
+
+tfInfo <- get_sample_information(
+  exptInfoFile = file_exptInfo,
+  samples = tfIds,
+  dataPath = TF_dataPath
+)
+
+polII_info <- get_sample_information(
+  exptInfoFile = file_exptInfo,
+  samples = unique(purrr::map(polIIDiffPairs, `[`, c("group2", "group1")) %>% unlist() %>% unname()),
+  dataPath = polII_dataPath
+)
+
+exptInfo <- dplyr::bind_rows(tfInfo, polII_info)
+exptInfoList <- purrr::transpose(exptInfo)  %>% 
+  purrr::set_names(nm = purrr::map(., "sampleId"))
+
+polII_ids <- exptInfo$sampleId[which(exptInfo$IP_tag == "polII")]
+tfIds <- exptInfo$sampleId[which(exptInfo$IP_tag %in% c("HA", "MYC", "TAP") & exptInfo$TF != "untagged")]
+
+
+polIICols <- list(
+  exp = structure(polII_ids, names = polII_ids),
+  is_expressed = structure(paste("is_expressed", ".", polII_ids, sep = ""), names = polII_ids)
+)
+
+
 tfCols <- sapply(
-  X = c("peakDist", "featureCovFrac", "hasPeak", "peakCoverage", "peakPosition", "peakId", "peakType",
-        "peakPval", "peakEnrichment", "preference", "peakCategory"),
-  FUN = function(x){ structure(paste(x, ".", tfInfo$sampleId, sep = ""), names = tfInfo$sampleId) },
+  c("hasPeak", "peakId", "peakEnrichment", "peakPval", "peakQval", "peakSummit", "peakDist", "summitDist",
+    "peakType", "bidirectional", "featureCovFrac", "relativeSummitPos", "peakRegion", "peakPosition",
+    "peakCoverage", "pvalFiltered", "summitSeq"),
+  FUN = function(x){ structure(paste(x, ".", tfIds, sep = ""), names = tfIds) },
   simplify = F, USE.NAMES = T)
 
 ##################################################################################
 ## prepare data for plotting
 tfBindingMat <- peak_target_matrix(sampleInfo = tfInfo, position = "best")
-polIIData <- get_polII_expressions(genesDf = geneSet, exptInfo = polII_info) %>% 
-  dplyr::mutate_at(.vars = polIICols$exp, .funs = list(~log2(pmax(., 1))))
+polIIData <- get_polII_expressions(genesDf = geneInfo, exptInfo = polII_info)
 
 i <- 1
 ## add fold change columns
 for (i in names(polIIDiffPairs)) {
-  polIIData <- get_fold_change(df = polIIData,
-                               nmt = polIIDiffPairs[[i]]$samples[2],
-                               dmt = polIIDiffPairs[[i]]$samples[1],
-                               newCol = polIIDiffPairs[[i]]$name,
-                               lfcLimit = 1,
-                               isExpressedCols = polIICols$is_expressed)
+  polIIData <- get_fold_change(
+    df = polIIData,
+    nmt = polIIDiffPairs[[i]]$group1,
+    dmt = polIIDiffPairs[[i]]$group2,
+    newCol = polIIDiffPairs[[i]]$comparison,
+    lfcLimit = 0.585,
+    isExpressedCols = polIICols$is_expressed
+  )
 }
 
 
-mergedData <- dplyr::left_join(x = polIIData, y = tfBindingMat, by = "gene") %>% 
+mergedData <- dplyr::left_join(x = polIIData, y = tfBindingMat, by = "geneId") %>% 
   dplyr::mutate_at(.vars = vars(starts_with("hasPeak.")), .funs = list(~if_else(is.na(.), FALSE, .))) %>% 
-  dplyr::filter(!is.na(SM_CLUSTER)) %>% 
+  dplyr::filter(!is.na(SM_ID)) %>% 
   dplyr::mutate(
-    SM_CLUSTER = gsub(pattern = "SM_cluster_", replacement = "", x = SM_CLUSTER, fixed = TRUE)
+    SM_ID = gsub(pattern = "cluster_", replacement = "", x = SM_ID, fixed = TRUE)
   ) %>% 
   dplyr::group_by(SM_CLUSTER) %>% 
   dplyr::mutate(index = 1:n()) %>% 
   dplyr::ungroup() %>% 
-  dplyr::select(gene, SM_CLUSTER, index, starts_with("hasPeak"), starts_with("peakPval"),
+  dplyr::select(geneId, SM_CLUSTER, index, starts_with("hasPeak"), starts_with("peakPval"),
                 unname(polIICols$exp), unname(polIICols$is_expressed),
-                unname(purrr::map_chr(polIIDiffPairs, "name")))
+                unname(purrr::map_chr(polIIDiffPairs, "comparison")))
 
+
+##################################################################################
+## polII FC box with KERS targets
+
+# mergedData <- dplyr::filter(mergedData, SM_CLUSTER %in% c("01", "02", "03", "04", "05"))
+
+## prepare peak data
+hasPeak <- pivot_longer(
+  data = mergedData,
+  cols = c(starts_with("hasPeak"), starts_with("peakPval")),
+  names_to = c(".value", "sampleId"),
+  names_sep = "\\.",
+  values_drop_na = TRUE
+)  %>% 
+  dplyr::filter(hasPeak == TRUE) %>% 
+  dplyr::select(geneId, SM_CLUSTER, index, sampleId, hasPeak, peakPval) %>% 
+  dplyr::mutate(
+    sampleId = forcats::fct_relevel(.f = sampleId, tfIds)
+  )
+
+
+tfPointPosition <- structure(
+  .Data = seq(0, 1, length.out = length(tfInfo$sampleId)+2)[2:(1+length(tfInfo$sampleId))],
+  names = tfInfo$sampleId
+)
+
+hasPeak$tfPosition <- tfPointPosition[hasPeak$sampleId]
+
+tfColor <- purrr::map_chr(
+  .x = exptInfoList[tfInfo$sampleId],
+  .f = function(x) unlist(strsplit(x = x$color, split = ","))[2]
+)
+
+
+## prepare polII FC data
+polIILfc <- pivot_longer(
+  data = mergedData,
+  cols = unname(purrr::map_chr(polIIDiffPairs, "comparison")),
+  names_to = "pair",
+  values_to = "lfc",
+  values_drop_na = TRUE
+)  %>% 
+  dplyr::select(geneId, SM_CLUSTER, index, pair, lfc) %>% 
+  dplyr::mutate(
+    pair = forcats::fct_relevel(.f = pair, unname(purrr::map_chr(polIIDiffPairs, "comparison")))
+  )
 
 
 ##################################################################################
@@ -143,49 +198,11 @@ ptTheme <- theme_bw() +
         strip.background = element_rect(fill="white", size = 0.2),
         legend.text = element_text(size = 13),
         legend.position = "bottom",
-        legend.direction = "vertical",
+        legend.direction = "horizontal",
         legend.title = element_text(size = 13, face = "bold"),
         plot.margin = unit(rep(0.5, 4), "cm"))
 
-
-##################################################################################
-## polII FC box with KERS targets
-
-# mergedData <- dplyr::filter(mergedData, SM_CLUSTER %in% c("01", "02", "03", "04", "05"))
-
-## prepare peak data
-hasPeak <- as.data.table(mergedData) %>% 
-  data.table::melt(id.vars = c("gene", "SM_CLUSTER", "index"),
-                   measure.vars = list(hasPeak = tfCols$hasPeak, peakPval = tfCols$peakPval),
-                   variable.name = "sampleId") %>% 
-  as_tibble() %>% 
-  dplyr::filter(hasPeak == TRUE)
-
-hasPeak$sampleId <- forcats::fct_recode(
-  .f = hasPeak$sampleId,
-  structure(levels(hasPeak$sampleId), names = tfInfo$sampleId)
-)
-
-tfPointPosition <- structure(seq(0, 1, length.out = length(tfInfo$sampleId)+2)[2:(1+length(tfInfo$sampleId))],
-                             names = tfInfo$sampleId)
-
-hasPeak$tfPosition <- tfPointPosition[hasPeak$sampleId]
-
-tfColor <- purrr::map_chr(.x = exptDataList[tfInfo$sampleId],
-                          .f = function(x) unlist(strsplit(x = x$color, split = ","))[2])
-
-
-## prepare polII FC data
-polIILfc <- as.data.table(mergedData) %>% 
-  data.table::melt(id.vars = c("gene", "SM_CLUSTER", "index"),
-                   measure.vars = list(lfc = unname(purrr::map_chr(polIIDiffPairs, "name"))),
-                   variable.name = "pair", value.name = "lfc") %>% 
-  as_tibble()
-
-polIILfc$pair <- forcats::fct_recode(
-  .f = polIILfc$pair,
-  structure(levels(polIILfc$pair), names = unname(purrr::map_chr(polIIDiffPairs, "name")))
-)
+plotTitle <- paste("SM genes | KERS targets |", polIIComparison)
 
 pt <- ggplot() +
   geom_tile(data = polIILfc, mapping = aes(x = 0.5, y = pair, fill = lfc)) +
@@ -197,10 +214,10 @@ pt <- ggplot() +
     low = "#B35806", mid = "#F7F7F7", high = "#542788", midpoint = 0
   ) +
   scale_color_manual(name = "KERS peak", values = tfColor) +
-  scale_x_continuous(expand = expand_scale(add = c(0.0, 0.0))) +
+  scale_x_continuous(expand = expansion(add = c(0.0, 0.0))) +
   scale_y_discrete(limits = c(levels(polIILfc$pair), "tf")) +
   ggtitle(plotTitle) +
-  facet_wrap(facets =  vars(gene), ncol = 30, strip.position = "left", dir = "v") +
+  facet_wrap(facets =  vars(geneId), ncol = 35, strip.position = "left", dir = "v") +
   ptTheme + theme(strip.text.y = element_blank())
 
 
